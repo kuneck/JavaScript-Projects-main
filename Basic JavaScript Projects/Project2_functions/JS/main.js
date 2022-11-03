c82d85fd4d58b6734c845dee46ff8c4d385250bb
kuneck <kuneck@gmail.com>
2022-11-03 20:24:33 +0000

    update

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,25 @@
+//setting functions and call
+function Thor(){
+    document.getElementById("text").innerHTML = "Thor"
+    }
+
+function Spiderman(){
+    document.getElementById("text").innerHTML = "Spider-Man"
+    }
+
+function Namor(){
+    document.getElementById("text").innerHTML = "Namor"
+    }
+
+function Flash(){
+    document.getElementById("text").innerHTML = "Flash"
+    }
+
+    var A = "Thor, ";
+    var B = "Spider-Man, "  
+    var C = "Namor and "
+    var D= "Flash."
+
+function answer(){
+    document.getElementById("text").innerHTML = A+B+C+D
+    }
