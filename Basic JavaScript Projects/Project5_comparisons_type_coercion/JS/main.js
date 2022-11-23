613dd7637fe194ec2a1856caff2db58974f3232e
kuneck <kuneck@gmail.com>
2022-11-23 20:14:31 +0000

    update

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,19 @@
+document.write(typeof "Test");
+
+document.write(2E310);
+
+document.write(-2E310);
+
+function positive(){
+    var test1 = 3E308 * 3;
+    document.getElementById("demo").innerHTML = "3E308 * 3 = " + test1;
+}
+
+function negative(){
+    var test2 = -3E308 * 3;
+    document.getElementById("demo2").innerHTML = "-3E308 * 3 = " + test2;
+}
+
+document.write(10 > 2)
+
+console.log(2+6);
